35eb33d3abcc6499da559cf17aad36546e4dc17c
민아영 <minay0715@naver.com>
2022-04-06 14:26:08 +0900

    + loginException

--- PaymentService/src/test/java/com/nhnacademy/paymentservice/AccountAvailableTest.java
+++ PaymentService/src/test/java/com/nhnacademy/paymentservice/AccountAvailableTest.java
@@ -3,15 +3,26 @@ package com.nhnacademy.paymentservice;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AccountAvailableTest {
 
     @BeforeEach
     void setUp() {
+        repository = mock(AccountRepository.class);
+        service = new AccountService(repository);
 
     }
 
+    @DisplayName("계정이 없으면 예외 발생.")
     @Test
+    void unableIdException () {
 
+        when(repository.findByUsername(username)).thenReturn(account); //조작하는 stubbing 과정
+
+        Account result = service.login(username, password);
+
+    }
 }
+
---
+++ PaymentService/src/test/java/com/nhnacademy/paymentservice/AccountServiceDummyTest.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.paymentservice;
+
+public class AccountServiceDummyTest {
+	private AccountService service;
+	private AccountRepository repository;
+
+
+
+}
